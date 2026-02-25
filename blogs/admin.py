414f7cc78d6612fd537c649316de97c176d2d582
Rana Usama <muhammad.usama.ashraf.se@gmail.com>
2026-02-25 12:50:48 +0500

    blogs apps and blog model

---
+++ blogs/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+
+from .models import Blog, Category
+
+# Register your models here.
+
+
+class BlogAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+    list_display = ("title", "category", "author", "status", "is_featured")
+    search_fields = (
+        "id",
+        "title",
+        "category__category_name",
+        "status",
+        "author__username",
+    )
+    list_editable = ("is_featured", "status")
+
+
+admin.site.register(Category)
+admin.site.register(Blog, BlogAdmin)
--- config/urls.py
+++ config/urls.py
@@ -15,9 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
 from . import views
 
-urlpatterns = [path("admin/", admin.site.urls), path("", views.home, name="home")]
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", views.home, name="home"),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
