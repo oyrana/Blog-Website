a93f02a5178f9d270c31db252679404d4bbf75ae
Rana Usama <muhammad.usama.ashraf.se@gmail.com>
2026-03-01 15:24:49 +0500

    posts by category

--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
+
+from .models import Blog, Category
 
 # Create your views here.
+
+
+def posts_by_category(request, category_id):
+    # fetch the posts that belong to that category_id
+    posts = Blog.objects.filter(status="Published", category=category_id)
+    # try:
+    # category = Category.objects.get(pk=category_id)
+    # except:
+    # return redirect("home")
+
+    category = get_object_or_404(Category, pk=category_id)
+    context = {
+        "posts": posts,
+        "category": category,
+    }
+    return render(request, "post_by_category.html", context)
--- config/views.py
+++ config/views.py
@@ -1,5 +1,17 @@
+from blogs.models import Blog, Category
 from django.shortcuts import render
 
 
 def home(request):
-    return render(request, "home.html")
+
+    featured_posts = Blog.objects.filter(is_featured=True).order_by("updated_at")
+    posts = Blog.objects.filter(
+        is_featured=False,
+        status="Published",
+    )
+    context = {
+        "featured_posts": featured_posts,
+        "posts": posts,
+    }
+
+    return render(request, "home.html", context)
